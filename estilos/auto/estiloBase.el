113f50a2e5348a9e2e46ccc00a20c7ea4d863641
MGautier <moisesgautiergomez@correo.ugr.es>
2015-07-01 13:53:05 +0200

    Añadiendo los fuentes del libro Introducción a la teoría de grafos

---
+++ estilos/auto/estiloBase.el
@@ -0,0 +1,28 @@
+(TeX-add-style-hook "estiloBase"
+ (lambda ()
+    (TeX-run-style-hooks
+     "charter"
+     "rotating"
+     "tikz"
+     "graphicx"
+     "fullpage"
+     "float"
+     "eurosym"
+     "caption"
+     "listings"
+     "supertabular"
+     "multirow"
+     "array"
+     "amsmath"
+     "amsthm"
+     "amssymb"
+     "amsfonts"
+     "hyperref"
+     "color"
+     "babel"
+     "spanish"
+     "fontenc"
+     "T1"
+     "inputenc"
+     "utf8")))
+
